bdfe37c2f0a264973520bba63acfc40407fa7a29
Personal <faisaltez@gmail.com>
2023-04-16 12:12:03 +0600

    Updated

--- dart/dart-type.dart
+++ dart/dart-type.dart
@@ -1,9 +1,9 @@
-main(){
-  // Data
-  // Data Type
-  // Primitive Data Type
-  // Variable
-  // strictly-typed
+main() {
+  /// Data
+  /// Data Type
+  /// Primitive Data Type - String, int, double, bool
+  /// Variable
+  /// strictly-typed
 
   /*
   1. string - text/multiple characters/letters like "Bangladesh"
@@ -13,17 +13,58 @@ main(){
   3. boolean - true/false
    */
 
-  // To Help A Banking Application
-
-  String accountHolderName = "Faisal A. Salam"; // string - set of characters
+  /// To Help A Banking Application
+  String FirstName = 'Faisal';
+  String LastName = 'A. Salam';
+  String accountHolderFullName =
+      FirstName + ' ' + LastName; // string - set of characters
+  String accountHolderAddress = "53/5, Muradpur, Chittagong";
+  String accountHolderCompanyName = '''KAF Tech BD
+  - Quality is our Priority.''';
   int accountHolderAge = 36; // integer number
   double totalAmount = 400000.50; // decimal number
   bool isAccountHolderBangladeshi = true; // boolean true/false
 
-  // Print all the above variable
-  print(accountHolderName);
+  /// Multiple Phone Numbers
+  List<String> phoneNumberList = [
+    '+8801812361494',
+    '+8801812361495',
+    '+8801812361496',
+    '+8801812361497'
+  ];
+
+  /// Map - dynamic
+  Map studentNameList = {
+    101: "Faisal",
+    102: "Fahad",
+    103: "Fahim",
+    104: "Fatima",
+    104: "Farzana",
+  };
+
+  /// Map - with data type
+  Map<int, String> studentNameRollWise = {
+    201: "Jhon",
+    202: "Doe",
+    203: "Cyndy",
+    204: "Jose",
+  };
+
+  /// Print all the above variable
+  print(accountHolderFullName);
+  print(accountHolderAddress);
+  print(accountHolderCompanyName);
   print(accountHolderAge);
   print(totalAmount);
   print(isAccountHolderBangladeshi);
+
+  print(phoneNumberList);
+  print(phoneNumberList[0]);
+
+  /// Map - dynamic
+  print(studentNameList);
+
+  /// Map - with data type
+  print(studentNameRollWise);
 
 }
--- dart/variable.dart
+++ dart/variable.dart
@@ -4,4 +4,21 @@ main(){
 
   age = 36;
   print(age);
+
+  /// Initial State
+  var userName = 'Faisal';
+  String personName = "Fahim";
+  dynamic accountName = 'AC-1231233';
+
+  /// Update
+  userName = "Fahd"; // 1213
+  personName = 'Melon'; // true
+  accountName = true;
+
+  print(userName);
+  print(personName);
+  print(accountName);
+
+
+
 }
